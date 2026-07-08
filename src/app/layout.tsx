82cf6ccf2834458295ef06e5627b640f6ddfe400
neo-ming <ming@ming-neoMacBook.lan>
2026-07-08 09:24:54 +0800

    add Google Analytics G-9PLQPP677L

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-9PLQPP677L"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9PLQPP677L');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
